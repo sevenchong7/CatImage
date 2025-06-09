da8c81fadbccae2f94af29d60c46fdaaf24d37cf
Seven <chongqihao2010@gmail.com>
2025-06-09 18:28:35 +0800

    add type

--- app/store/catStore.tsx
+++ app/store/catStore.tsx
@@ -1,20 +1,27 @@
-'use client';
 import { create } from 'zustand';
+
+export type CatImage = {
+    id: string;
+    url: string;
+    height: number;
+    width: number;
+}
+
 export type State = {
-    catImage: any[];
-    favoriteCatImage: any[];
+    catImage: CatImage[];
+    favoriteCatImage: CatImage[];
 }
 
 export type Action = {
-    setCatImage: (image: any) => void;
-    setFavoriteCatImage: (image: any) => void;
+    setCatImage: (images: CatImage[]) => void;
+    setFavoriteCatImage: (image: CatImage) => void;
 }
 
 // Zustand store for managing cat images
 export const useCatStore = create<State & Action>()((set) => ({
     catImage: [],
     favoriteCatImage: [],
-    setCatImage: (image: any) => set({ catImage: image }),
+    setCatImage: (images) => set({ catImage: images }),
     setFavoriteCatImage: (image) => set((state) => ({
         favoriteCatImage: [...state.favoriteCatImage, image]
     })),
